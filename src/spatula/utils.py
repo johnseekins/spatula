4fe67bd90bfc57cb6629cac42c69d1f30c27ac24
John Seekins <john@civiceagle.com>
2022-06-24 13:30:10 -0600

    add initial pass at stats collection and actually include poetry.lock file
    Signed-off-by: John Seekins <john@civiceagle.com>

--- src/spatula/utils.py
+++ src/spatula/utils.py
@@ -1,7 +1,13 @@
-import pprint
-import typing
 import dataclasses
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client.client.write.point import Point
+from influxdb_client.domain.write_precision import WritePrecision
 from lxml.etree import _Element  # type: ignore
+import os
+import pprint
+import time
+import typing
 
 # utilities for working with optional dependencies
 try:
@@ -57,3 +63,53 @@ def _obj_to_dict(obj: typing.Any) -> typing.Optional[typing.Dict]:
         return obj.dict()
     else:
         raise ValueError(f"invalid type: {obj} ({type(obj)})")
+
+
+def write_stats(
+    metrics: list, prefix: typing.Optional[str] = "", org: typing.Optional[str] = ""
+) -> None:
+    """
+    A metric is a list of objects that look like:
+    {
+      "metric": "votes",
+      "tags": {
+        "jurisdiction": "ca"
+      },
+      "fields": {
+        "passed": 12,
+        "vetoed": 1,
+        "failed": 2,
+        "collected": 15
+      }
+    }
+
+    Our example turns into the following metrics:
+    votes_passed{jurisdiction="ca"} == 12
+    votes_vetoed{jurisdiction="ca"} == 1
+    votes_failed{jurisdiction="ca"} == 2
+    votes_collected{jurisdiction="ca"} == 15
+    """
+    ts = time.time()
+    points = list()
+    for m in metrics:
+        p = Point(f"{prefix}{m['metric']}")
+        p.time(int(m.get("timestamp", ts)), WritePrecision.S)
+        """
+        use list comprehensions 'cause they're technically faster than for loops
+        But this is simply turning a dictionary of k=v pairs into tags/fields
+        in the point object
+        """
+        [p.tag(t, v) for t, v in m.get("tags", {}).items()]
+        [p.field(f, v) for f, v in m["fields"].items()]
+        points.append(p)
+    try:
+        client = InfluxDBClient(
+            url=os.environ.get("INFLUX_ENDPOINT"),
+            token=os.environ.get("INFLUX_TOKEN"),
+            org=org,
+            enable_gzip=True,
+        )
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+        write_api.write("", record=points)
+    except Exception:
+        pass
